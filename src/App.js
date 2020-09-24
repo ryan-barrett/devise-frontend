6f29468188a807ba8b7e13141dd6542c06f1cc81
Ryan Barrett <ryan.barrett@spookyhouse.org>
2020-09-23 18:32:41 -0700

    add basic routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React          from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+}                     from 'react-router-dom';
 
 import './App.css';
 import BoardContainer from './containers/BoardContainer';
@@ -6,7 +12,16 @@ import BoardContainer from './containers/BoardContainer';
 function App() {
   return (
     <div className="App">
-      <BoardContainer boardId="board-5178f6b2-994c-47d8-bb0f-a1a778f016e9213"/>
+      <Router>
+        <Switch>
+          <Route path="/login">
+            <div>a login page</div>
+          </Route>
+          <Route path="/">
+            <BoardContainer boardId="board-5178f6b2-994c-47d8-bb0f-a1a778f016e9213"/>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
